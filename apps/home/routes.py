4418fe96b6f64aa21f0d2db063eb2fd223fbeb41
iffatainiyyah <iffatainiyyah@gmail.com>
2022-10-14 22:34:30 +0800

    fix error and form wc

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 
 from flask_sqlalchemy import SQLAlchemy
-from apps.home.website import symbol_wc, grouped_wc, vehicle_wc, regex_split, one_hot, euclideanDistance, prediksi
+from apps.home.website import regex_split, one_hot, euclideanDistance, prediksi
 from apps.home.kmedoids import k_medoids
 from apps.home import blueprint
 from flask import Flask, render_template, request, redirect, url_for, session
@@ -12,9 +12,15 @@ import os
 import pandas as pd
 import numpy as np
 import re
+import io
+import urllib
+import base64
 import pickle
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.decomposition import PCA 
+from wordcloud import WordCloud, STOPWORDS
+
 
 UPLOAD_FOLDER = '/file'
 ALLOWED_EXTENSIONS = {'csv', 'xls','xlsx'}
@@ -44,7 +50,7 @@ app.config['UPLOAD_FOLDER'] = '/Skripsi/Website/flask dashboard/apps/file'
 @blueprint.route('/addData.html',  methods=['GET','POST'])
 @login_required
 def uploadFile():
-    global data_pelanggaran
+    # global data_pelanggaran
     if request.method == 'POST':
         if request.files:
             file = request.files["file"]           
@@ -52,17 +58,14 @@ def uploadFile():
             ext = file.filename.rsplit('.', 1)[1].lower()
             acceptedExt = ['csv','xls','xlsx']
             if ext in acceptedExt:
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'],file.filename))
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'],'dataset test.xlsx'))
         
-        path = "/Skripsi/Website/flask dashboard/apps/file/"+(file.filename)
-        if ext == "csv":
-            data_pelanggaran = pd.read_csv(path)
-        else:
-            data_pelanggaran = pd.read_excel(path)
+        # path = "/Skripsi/Website/flask dashboard/apps/file/"+(file.filename)
+        # if ext == "csv":
+        #     data_pelanggaran = pd.read_csv(path)
+        # else:
+        #     data_pelanggaran = pd.read_excel(path)
         
-        # Rename value jenis kendaraan
-        data_pelanggaran['jenis_kendaraan'].replace(to_replace={'MOBIL BARANG/PICK UP':'MOBIL BARANG', 'LAIN-LAIN':'LAINNYA','MKL/MOBIL PENUMPANG':'MOBIL PENUMPANG', 
-                                           'TRUCK BESAR':'TRUK BESAR', 'TRUCK KECIL':'TRUK KECIL', 'TRUCK TANGKI':'TRUK TANGKI'}, inplace= True)
     
         
         return redirect(url_for('home_blueprint.datapelanggaran'))
@@ -83,6 +86,7 @@ def preprocessing():
     global data_pelanggaran
 
     data_pelanggaran = pd.DataFrame(data_pelanggaran)
+    print('ini data pelanggaran :',data_pelanggaran)
     data_prepro = data_pelanggaran.copy()
 
     # Split data
@@ -96,7 +100,7 @@ def preprocessing():
 
     data_prepro['pasal_hukuman'] = pasal_hukuman_series
     data_prepro['pasal_tuntutan'] = pasal_tuntutan_series
-    print(data_prepro)
+    print('ini hasil upload',data_prepro)
 
     data_numeric = data_prepro.copy()
     # One Hot Encoding data
@@ -108,8 +112,8 @@ def preprocessing():
     
     
     # Label Encoder 
-    jenis_kendaraan_dict = {'JEEP': 0, 'LAINNYA': 1, 'MINI BUS': 2, 'MOBIL PENUMPANG': 3, 'MOBIL BARANG': 4, 'SEDAN': 5, 
-                        'SEPEDA MOTOR': 6, 'TRUK BESAR': 7, 'TRUK KECIL': 8, 'TRUK TANGKI': 9}
+    jenis_kendaraan_dict = {'JEEP': 0, 'LAIN-LAIN': 1, 'MINI BUS': 2, 'MKL/MOBIL PENUMPANG': 3, 'MOBIL BARANG/PICK UP': 4, 'SEDAN': 5, 
+                        'SEPEDA MOTOR': 6, 'TRUCK BESAR': 7, 'TRUCK KECIL': 8, 'TRUCK TANGKI': 9}
 
     data_numeric['jenis_kendaraan'] = data_numeric.jenis_kendaraan.map(jenis_kendaraan_dict)
     
@@ -159,7 +163,7 @@ def clustering():
 def datacluster():
     global data_prepro
     global predict_cluster
-
+    global data_visual
     # Rename data pasal
     data_prepro = pd.DataFrame(data_prepro)
     data_visual = data_prepro.copy()
@@ -233,126 +237,103 @@ def datacluster():
     data_visual['pasal_tuntutan'] = np.where(data_visual['pasal_tuntutan_2'].isnull(), data_visual['pasal_tuntutan_1'], 
                                 data_visual['pasal_tuntutan_1']+', '+ data_visual['pasal_tuntutan_2'])
     
+     # Rename value jenis kendaraan
+    data_visual['jenis_kendaraan'].replace(to_replace={'MOBIL BARANG/PICK UP':'MOBIL BARANG', 'LAIN-LAIN':'LAINNYA','MKL/MOBIL PENUMPANG':'MOBIL PENUMPANG', 
+                                           'TRUCK BESAR':'TRUK BESAR', 'TRUCK KECIL':'TRUK KECIL', 'TRUCK TANGKI':'TRUK TANGKI'}, inplace= True)
+    
     data_visual.drop('pasal_tuntutan_1', axis=1, inplace=True)
     data_visual.drop('pasal_tuntutan_2', axis=1, inplace=True)
     data_visual.drop('pasal_hukuman_1', axis=1, inplace=True)
     data_visual.drop('pasal_hukuman_2', axis=1, inplace=True)
 
     data_visual['cluster'] = predict_cluster
-    print(data_visual)
-
+    
+    
     return render_template('home/dataCluster.html', segment='pelanggaran', data_visual=data_visual)
 
+def symbols_wc(columnn,cluster):
+    global data_visual
+    data_visual = pd.DataFrame(data_visual)
+
+    # Get Data
+    cluster = int(cluster)
+    cluster0 = data_visual.loc[data_visual['cluster'] == cluster]
+    cluster1 = cluster0[columnn].tolist()
+    stopwords = set(STOPWORDS)
+
+    dictionary={}
+    for word in cluster1:
+        if word not in dictionary.keys():
+            dictionary[word] = 1
+        else:
+            dictionary[word] += 1
+        
+        
+    cloud = WordCloud(width = 1200, height = 800,
+                            background_color = '#FFFDE3',
+                            colormap='tab10',
+				            stopwords = stopwords,
+				            min_font_size = 16)
+    cloud.generate_from_frequencies(dictionary)
+        
+    plt.figure(figsize = (5, 5), facecolor = None)
+    plt.imshow(cloud)
+    plt.axis("off")
+    plt.tight_layout(pad = 0)
+    # plt.savefig('generated_plots.png')
+
+    img = io.BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plot_data = urllib.parse.quote(base64.b64encode(img.getvalue()).decode('utf-8'))
+    return plot_data
 
 
+def vehicles_wc(column,cluster):
+    global data_visual
+    data_visual = pd.DataFrame(data_visual)
 
-@blueprint.route('/word-cloud.html')
-@login_required
-def wc():
-    # symbol_wc(cluster ke berapa, nama tabelnya)
-    # word cloud singkatan
-    plot_data_0a = symbol_wc("pasal_tuntutan", 0, "Cluster_s")
-    plot_data_1a = symbol_wc("pasal_tuntutan", 1, "Cluster_s")
-    plot_data_2a = symbol_wc("pasal_tuntutan", 2, "Cluster_s")
-    plot_data_3a = symbol_wc("pasal_tuntutan", 3, "Cluster_s")
-    plot_data_4a = symbol_wc("pasal_tuntutan", 4, "Cluster_s")
-    plot_data_5a = symbol_wc("pasal_tuntutan", 5, "Cluster_s")
-    plot_data_6a = symbol_wc("pasal_tuntutan", 6, "Cluster_s")
-    plot_data_7a = symbol_wc("pasal_tuntutan", 7, "Cluster_s")
-    plot_data_8a = symbol_wc("pasal_tuntutan", 8, "Cluster_s")
-
-    plot_data_0b = symbol_wc("pasal_hukuman", 0, "Cluster_s")
-    plot_data_1b = symbol_wc("pasal_hukuman", 1, "Cluster_s")
-    plot_data_2b = symbol_wc("pasal_hukuman", 2, "Cluster_s")
-    plot_data_3b = symbol_wc("pasal_hukuman", 3, "Cluster_s")
-    plot_data_4b = symbol_wc("pasal_hukuman", 4, "Cluster_s")
-    plot_data_5b = symbol_wc("pasal_hukuman", 5, "Cluster_s")
-    plot_data_6b = symbol_wc("pasal_hukuman", 6, "Cluster_s")
-    plot_data_7b = symbol_wc("pasal_hukuman", 7, "Cluster_s")
-    plot_data_8b = symbol_wc("pasal_hukuman", 8, "Cluster_s")
-    
-    plot_data_0c = vehicle_wc("jenis_kendaraan", 0)
-    plot_data_1c = vehicle_wc("jenis_kendaraan", 1)
-    plot_data_2c = vehicle_wc("jenis_kendaraan", 2)
-    plot_data_3c = vehicle_wc("jenis_kendaraan", 3)
-    plot_data_4c = vehicle_wc("jenis_kendaraan", 4)
-    plot_data_5c = vehicle_wc("jenis_kendaraan", 5)
-    plot_data_6c = vehicle_wc("jenis_kendaraan", 6)
-    plot_data_7c = vehicle_wc("jenis_kendaraan", 7)
-    plot_data_8c = vehicle_wc("jenis_kendaraan", 8)
+    # Get Data
+    cluster = int(cluster)
+    cluster0 = data_visual.loc[data_visual['cluster'] == cluster]
+    cluster1 = cluster0[column]
+    words = ''
+    stopwords = set(STOPWORDS)
 
 
+    for val in cluster1:
 
-    return render_template('home/word-cloud.html', segment='word-cloud',
-    plot_cluster0a=plot_data_0a, 
-    plot_cluster1a=plot_data_1a,
-    plot_cluster2a=plot_data_2a,
-    plot_cluster3a=plot_data_3a,
-    plot_cluster4a=plot_data_4a,
-    plot_cluster5a=plot_data_5a,
-    plot_cluster6a=plot_data_6a,
-    plot_cluster7a=plot_data_7a,
-    plot_cluster8a=plot_data_8a,
+        # typecaste each val to string
+        val = str(val)
 
-    plot_cluster0b=plot_data_0b,
-    plot_cluster1b=plot_data_1b,
-    plot_cluster2b=plot_data_2b,
-    plot_cluster3b=plot_data_3b,
-    plot_cluster4b=plot_data_4b,
-    plot_cluster5b=plot_data_5b,
-    plot_cluster6b=plot_data_6b,
-    plot_cluster7b=plot_data_7b,
-    plot_cluster8b=plot_data_8b,
+        # split the value
+        tokens = val.split()
 
-    plot_cluster0c=plot_data_0c,
-    plot_cluster1c=plot_data_1c,
-    plot_cluster2c=plot_data_2c,
-    plot_cluster3c=plot_data_3c,
-    plot_cluster4c=plot_data_4c,
-    plot_cluster5c=plot_data_5c,
-    plot_cluster6c=plot_data_6c,
-    plot_cluster7c=plot_data_7c,
-    plot_cluster8c=plot_data_8c
-    )
+        # Converts each token into lowercase
+        for i in range(len(tokens)):
+            tokens[i] = tokens[i].lower()
+        
+        words += " ".join(tokens)+" "
+        
+        
+    wordcloud = WordCloud(width = 1200, height = 800,
+                            background_color = '#FFFDE3',
+                            colormap='tab10',
+				            stopwords = stopwords,
+				            min_font_size = 16).generate(words)
 
+        
+    plt.figure(figsize = (5, 5), facecolor = None)
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.tight_layout(pad = 0)
+    # plt.savefig('generated_plots.png')
 
-@blueprint.route('/<template>')
-@login_required
-def route_template(template):
-
-    try:
-
-        if not template.endswith('.html'):
-            template += '.html'
-
-        # Detect the current page
-        segment = get_segment(request)
-
-        # Serve the file (if exists) from app/templates/home/FILE.html
-        return render_template("home/" + template, segment=segment)
-
-    except TemplateNotFound:
-        return render_template('home/page-404.html'), 404
-
-    except:
-        return render_template('home/page-500.html'), 500
-
-
-# Helper - Extract current page name from request
-def get_segment(request):
-
-    try:
-
-        segment = request.path.split('/')[-1]
-
-        if segment == '':
-            segment = 'index'
-
-        return segment
-
-    except:
-        return None
-
+    img = io.BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plot_data = urllib.parse.quote(base64.b64encode(img.getvalue()).decode('utf-8'))
+    return plot_data
 
 @blueprint.route('/form-wc.html', methods=['GET','POST'])
 @login_required
@@ -360,17 +341,16 @@ def wcs():
     if request.method == 'POST':
         columns = request.form.get('column')
         clusters = request.form.get('cluster')
-        tables = request.form.get('table')
         details_t = ["Pasal 106 ayat (5) huruf a = Pada saat diadakan pemeriksaan Kendaraan Bermotor di Jalan setiap orang yang mengemudikan Kendaraan Bermotor wajib menunjukkan Surat Tanda Nomor Kendaraan Bermotor atau Surat Tanda Coba Kendaraan Bermotor.", "Pasal 106 ayat (5) huruf b = Pada saat diadakan pemeriksaan Kendaraan Bermotor di Jalan setiap orang yang mengemudikan Kendaraan Bermotor wajib menunjukkan Surat Izin Mengemudi (SIM).", "Pasal 77 ayat (1) = Setiap orang yang mengemudikan Kendaraan Bermotor di Jalan wajib memiliki Surat Izin Mengemudi sesuai dengan jenis Kendaraan Bermotor yang dikemudikan.", "Pasal 106 ayat (4) huruf c = Setiap orang yang mengemudikan Kendaraan Bermotor di Jalan wajib mematuhi ketentuan Alat Pemberi Isyarat Lalu Lintas.", "Pasal 68 ayat (1) = Setiap Kendaraan Bermotor yang dioperasikan di Jalan wajib dilengkapi dengan Surat Tanda Nomor Kendaraan Bermotor dan Tanda Nomor Kendaraan Bermotor.", "Pasal 106 ayat (4) huruf a =  Setiap orang yang mengemudikan Kendaraan Bermotor di Jalan wajib mematuhi ketentuan rambu perintah atau rambu larangan dan Pasal 106 ayat (4) huruf b =  Setiap orang yang mengemudikan Kendaraan Bermotor di Jalan wajib mematuhi ketentuan Marka Jalan. ", "Pasal 106 ayat (6) = Setiap orang yang mengemudikan Kendaraan Bermotor beroda empat atau lebih di Jalan dan penumpang yang duduk di sampingnya wajib mengenakan sabuk keselamatan. ", "Pasal 106 ayat (8) = Setiap orang yang mengemudikan Sepeda Motor dan Penumpang Sepeda Motor wajib mengenakan helm yang memenuhi standar nasional Indonesia.", "Pasal 106 ayat (3) =Setiap orang yang mengemudikan Kendaraan Bermotor di Jalan wajib mematuhi ketentuan tentang persyaratan teknis dan laik jalan dan Pasal 106 ayat (5) huruf c = Pada saat diadakan pemeriksaan Kendaraan Bermotor di Jalan setiap orang yang mengemudikan Kendaraan Bermotor wajib menunjukkan bukti lulus uji berkala "]
         details_h = ["Pasal 288 ayat (1) = Setiap orang yang mengemudikan Kendaraan Bermotor di Jalan yang tidak dilengkapi dengan Surat Tanda Nomor Kendaraan Bermotor atau Surat Tanda Coba Kendaraan Bermotor yang ditetapkan oleh Kepolisian Negara Republik Indonesia sebagaimana dimaksud dalam Pasal 106 ayat (5) huruf a dipidana dengan pidana kurungan paling lama 2 (dua) bulan atau denda paling banyak Rp500.000,00 (lima ratus ribu rupiah).", "Pasal 288 ayat (2) = Setiap orang yang mengemudikan Kendaraan Bermotor di Jalan yang tidak dapat menunjukkan Surat Izin Mengemudi yang sah sebagaimana dimaksud dalam Pasal 106 ayat (5) huruf b dipidana dengan pidana kurungan paling lama 1 (satu) bulan dan/atau denda paling banyak Rp250.000,00 (dua ratus lima puluh ribu rupiah).", "Pasal 281 = Setiap orang yang mengemudikan Kendaraan Bermotor di Jalan yang tidak memiliki Surat Izin Mengemudi sebagaimana dimaksud dalam Pasal 77 ayat (1) dipidana dengan pidana kurungan paling lama 4 (empat) bulan atau denda paling banyak Rp1.000.000,00 (satu juta rupiah).", "Pasal 287 ayat (2) = Setiap orang yang mengemudikan Kendaraan Bermotor di Jalan yang melanggar aturan perintah atau larangan yang dinyatakan dengan Alat Pemberi Isyarat Lalu Lintas sebagaimana dimaksud dalam Pasal 106 ayat (4) huruf c dipidana dengan pidana kurungan paling lama 2 (dua) bulan atau denda paling banyak Rp500.000,00 (lima ratus ribu rupiah).", "Pasal 280 = Setiap orang yang mengemudikan Kendaraan Bermotor di Jalan yang tidak dipasangi Tanda Nomor Kendaraan Bermotor yang ditetapkan oleh Kepolisian Negara Republik Indonesia sebagaimana dimaksud dalam Pasal 68 ayat (1) dipidana dengan pidana kurungan paling lama 2 (dua) bulan atau denda paling banyak Rp500.000,00 (lima ratus ribu rupiah).", "Pasal 287 ayat (1) = Setiap orang yang mengemudikan Kendaraan Bermotor di Jalan yang melanggar aturan perintah atau larangan yang dinyatakan dengan Rambu Lalu Lintas sebagaimana dimaksud dalam Pasal 106 ayat (4) huruf a atau Marka Jalan sebagaimana dimaksud dalam Pasal 106 ayat (4) huruf b dipidana dengan pidana kurungan paling lama 2 (dua) bulan atau denda paling banyak Rp500.000,00 (lima ratus ribu rupiah).", "Pasal 289 = Setiap orang yang mengemudikan Kendaraan Bermotor atau Penumpang yang duduk di samping Pengemudi yang tidak mengenakan sabuk keselamatan sebagaimana dimaksud dalam Pasal 106 ayat (6) dipidana dengan pidana kurungan paling lama 1 (satu) bulan atau denda paling banyak Rp250.000,00 (dua ratus lima puluh ribu rupiah).", "Pasal 291 ayat (1) = Setiap orang yang mengemudikan Sepeda Motor tidak mengenakan helm standar nasional Indonesia sebagaimana dimaksud dalam Pasal 106 ayat (8) dipidana dengan pidana kurungan paling lama 1 (satu) bulan atau denda paling banyak Rp250.000,00 (dua ratus lima puluh ribu rupiah) dan Pasal 291 ayat (2) = Setiap orang yang mengemudikan Sepeda Motor yang membiarkan penumpangnya tidak mengenakan helm sebagaimana dimaksud dalam Pasal 106 ayat (8) dipidana dengan pidana kurungan paling lama 1 (satu) bulan atau denda paling banyak Rp250.000,00 (dua ratus lima puluh ribu rupiah).", "Pasal 285 ayat (1) = Setiap orang yang mengemudikan Sepeda Motor di Jalan yang tidak memenuhi persyaratan teknis dan laik jalan yang meliputi kaca spion, klakson, lampu utama, lampu rem, lampu penunjuk arah, alat pemantul cahaya, alat pengukur kecepatan, knalpot, dan kedalaman alur ban sebagaimana dimaksud dalam Pasal 106 ayat (3) juncto Pasal 48 ayat (2) dan ayat (3) dipidana dengan pidana kurungan paling lama 1 (satu) bulan atau denda paling banyak Rp250.000,00 (dua ratus lima puluh ribu rupiah) dan Pasal 288 ayat (3) = Setiap orang yang mengemudikan mobil penumpang umum, mobil bus, mobil barang, kereta gandengan, dan kereta tempelan yang tidak dilengkapi dengan surat keterangan uji berkala dan tanda lulus uji berkala sebagaimana dimaksud dalam Pasal 106 ayat (5) huruf c dipidana dengan pidana kurungan paling lama 2 (dua) bulan atau denda paling banyak Rp500.000,00 (lima ratus ribu rupiah)."]
 
         # condition for input
-        if tables == 'Cluster_s':
-            result = symbol_wc(columns, clusters, tables)
-        elif tables == 'Cluster_a':
-            result = grouped_wc(columns, clusters, tables)
+        if columns == 'pasal_hukuman':
+            result = symbols_wc(columns, clusters)
+        elif columns == 'pasal_tuntutan':
+            result = symbols_wc(columns, clusters)
         elif columns == 'jenis_kendaraan':
-            result = vehicle_wc(columns, clusters)
+            result = vehicles_wc(columns, clusters)
         
         # condition for details pasal tuntutan
         if columns == 'pasal_tuntutan' and clusters == '0':
@@ -420,7 +400,46 @@ def wcs():
 
 
         return render_template('home/form-wc.html', plot_forms = result, plot_details = details, plot_columns = columns,
-                                plot_clusters = clusters, plot_tables = tables, segment="form")
-
+                                plot_clusters = clusters, segment="form")
 
     return render_template('home/form.html', plot_forms, plot_details, segment="form")
+
+
+
+@blueprint.route('/<template>')
+@login_required
+def route_template(template):
+
+    try:
+
+        if not template.endswith('.html'):
+            template += '.html'
+
+        # Detect the current page
+        segment = get_segment(request)
+
+        # Serve the file (if exists) from app/templates/home/FILE.html
+        return render_template("home/" + template, segment=segment)
+
+    except TemplateNotFound:
+        return render_template('home/page-404.html'), 404
+
+    except:
+        return render_template('home/page-500.html'), 500
+
+
+# Helper - Extract current page name from request
+def get_segment(request):
+
+    try:
+
+        segment = request.path.split('/')[-1]
+
+        if segment == '':
+            segment = 'index'
+
+        return segment
+
+    except:
+        return None
+
--- apps/home/website.py
+++ apps/home/website.py
@@ -10,150 +10,13 @@ import random
 import re
 import numpy as np
 
-app = Flask(__name__)
+
 
 
 
 def home_page():
     return render_template('index.html')
 
-
-def symbol_wc(df, column,cluster):
-
-    # Get Data
-    cluster0 = df.loc[df[column] == cluster]
-    words = ''
-    stopwords = set(STOPWORDS)
-
-
-    for val in cluster0:
-
-        # typecaste each val to string
-        val = str(val)
-
-        # split the value
-        tokens = val.split()
-
-        # Converts each token into lowercase
-        for i in range(len(tokens)):
-            tokens[i] = tokens[i].lower()
-        
-        words += " ".join(tokens)+" "
-        
-        
-    wordcloud = WordCloud(width = 1200, height = 800,
-                            background_color = '#FFFDE3',
-                            colormap='tab10',
-				            stopwords = stopwords,
-                            regexp = r"[Pp]s[0-9](?:[\(]?[0-9]*[\)]?)?(?:[\(]?[0-9]*[\)]?)? ? ?[a-zA-Z]?",
-				            min_font_size = 16).generate(words)
-
-        
-    plt.figure(figsize = (5, 5), facecolor = None)
-    plt.imshow(wordcloud)
-    plt.axis("off")
-    plt.tight_layout(pad = 0)
-    # plt.savefig('generated_plots.png')
-
-    img = io.BytesIO()
-    plt.savefig(img, format='png')
-    img.seek(0)
-    plot_data = urllib.parse.quote(base64.b64encode(img.getvalue()).decode('utf-8'))
-    return plot_data
-
-
-def grouped_wc(df,column,cluster):
-
-    # Get Data
-    cluster0 = df.loc[df[column] == cluster]
-    grouped_words = []
-    dictionary = {}
-    stopwords = set(STOPWORDS)
-
-
-    for val in cluster0:
-
-        # typecaste each val to string
-        vals = val[0]
-
-        # split the value and grouped 3 words
-        tokens = vals.split()
-        grouped_words += [' '.join(tokens[i: i + 3]) for i in range(0, len(tokens), 3)]
-
-       # Converts each words into lowercase
-        for i in range(len(grouped_words)):
-            grouped_words[i] = grouped_words[i].lower()
-            # grouped_words[i] = re.sub(r'[^\w\s\,]', '', grouped_words[i])
-            grouped_words[i] = grouped_words[i].replace(',','')
-        
-    for word in grouped_words:
-        if word not in dictionary.keys():
-            dictionary[word] = 1
-        else:
-            dictionary[word] += 1
-
-    cloud = WordCloud(width = 1200, height = 800,
-                            background_color = '#FFFDE3',
-                            colormap='tab10',
-				            stopwords = stopwords,
-				            min_font_size = 16)
-    cloud.generate_from_frequencies(dictionary)
-
-        
-    plt.figure(figsize = (5, 5), facecolor = None)
-    plt.imshow(cloud)
-    plt.axis("off")
-    plt.tight_layout(pad = 0)
-    # plt.savefig('generated_plots.png')
-
-    img = io.BytesIO()
-    plt.savefig(img, format='png')
-    img.seek(0)
-    plot_data = urllib.parse.quote(base64.b64encode(img.getvalue()).decode('utf-8'))
-    return plot_data
-
-
-def vehicle_wc(df,column,cluster):
-
-    # Get Data
-    cluster0 = df.loc[df[column] == cluster]
-    words = ''
-    stopwords = set(STOPWORDS)
-
-    for val in cluster0:
-
-        
-        vals = val[0]
-
-        # split the value
-        tokens = vals.split()
-
-        # Converts each token into lowercase
-        for i in range(len(tokens)):
-            tokens[i] = tokens[i].lower()
-        
-        words += " ".join(tokens)+" "
-        
-        
-    wordcloud = WordCloud(width = 1200, height = 800,
-                            background_color = '#FFFDE3',
-                            colormap='tab10',
-				            stopwords = stopwords,
-                            # regexp=r"[a-z]{0,10}?[a-z]{0,10}",
-				            min_font_size = 10).generate(words)
-
-        
-    plt.figure(figsize = (5, 5), facecolor = None)
-    plt.imshow(wordcloud)
-    plt.axis("off")
-    plt.tight_layout(pad = 0)
-    # plt.savefig('generated_plots.png')
-
-    img = io.BytesIO()
-    plt.savefig(img, format='png')
-    img.seek(0)
-    plot_data = urllib.parse.quote(base64.b64encode(img.getvalue()).decode('utf-8'))
-    return plot_data
 
 def regex_split(series, pattern):
     data = []
@@ -208,5 +71,3 @@ def prediksi(medoid, data):
     return np.array(pred)
     
 
-if __name__ == "__main__":
-    app.run(debug=True)
